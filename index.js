0386f08da481110146f2b474f4ca3088d7ecb568
rimakhatun3 <rimamnt46@gmail.com>
2023-05-02 13:06:12 +0600

    added json data

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const cors = require('cors')
+const chefData = require('./data/ChefData.json')
+
+app.use(cors())
+app.get('/',(req,res)=>{
+    res.send('api is running')
+})
+
+app.get('/chef',(req,res)=>{
+    res.send(chefData)
+})
+
+app.get('/chef/:id',(req,res)=>{
+    const id = req.params.id;
+
+    const findedData = chefData.find(pd=>pd.id===id)
+    res.send(findedData)
+})
+
+
+app.listen(port,()=>{
+    console.log(`port is running on:${port}`)
+})
